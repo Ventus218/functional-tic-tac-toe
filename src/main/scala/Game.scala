e683a9dc1402baf2d4faaefeeec75298ab123301
Alessandro Venturini <ales.ventus21@gmail.com>
2024-05-19 16:57:03 +0200

    Added apply

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -7,12 +7,16 @@ object Game:
       availableMoves: Set[Position]
   )
 
-extension (game: Game)
-  def makeMove(move: (HorizontalPosition, VerticalPosition)): Game =
-    require(game.nextPlayer.isDefined)
-    require(game.availableMoves.contains(move))
-    Game(
-      game.grid.place(move, game.nextPlayer.get),
-      game.nextPlayer.map(_.other),
-      game.availableMoves - move
-    )
+  def apply(firstPlayer: Player): Game =
+    val grid = Grid()
+    Game(grid, Option(firstPlayer), grid.emptyPositions)
+
+  extension (game: Game)
+    def makeMove(move: Position): Game =
+      require(game.nextPlayer.isDefined)
+      require(game.availableMoves.contains(move))
+      Game(
+        game.grid.place(move, game.nextPlayer.get),
+        game.nextPlayer.map(_.other),
+        game.availableMoves - move
+      )
