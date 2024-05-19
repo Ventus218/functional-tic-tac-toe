502d7cd56cc678e19136a757707212e6ae8d6d54
Alessandro Venturini <ales.ventus21@gmail.com>
2024-05-19 16:25:56 +0200

    Grid refactor, added player on each cell

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,11 +1,19 @@
 object Grid:
-  opaque type Grid = Set[(HorizontalPosition, VerticalPosition)]
+  opaque type Grid = Map[(HorizontalPosition, VerticalPosition), Option[Player]]
 
   def apply(): Grid =
-    (for
+    val positions = for
       x <- HorizontalPosition.values
       y <- VerticalPosition.values
-    yield (x, y)).toSet
+    yield (x, y)
+    Map.from(positions.map(pos => (pos, Option.empty)))
+
+  extension (grid: Grid)
+    def place(
+        pos: (HorizontalPosition, VerticalPosition),
+        player: Player
+    ): Grid =
+      grid.+((pos, Option(player)))
 
 enum HorizontalPosition:
   case Left, Center, Right
