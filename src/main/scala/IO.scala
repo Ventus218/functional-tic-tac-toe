51c9fec242ef43634fc1b279ce686b69a3dc41dc
Alessandro Venturini <ales.ventus21@gmail.com>
2024-05-26 19:15:13 +0200

    moved apply and used it where possible

--- src/main/scala/IO.scala
+++ src/main/scala/IO.scala
@@ -5,8 +5,6 @@ object IO:
   opaque type IO[T] = IOImpl[T]
   case class IOImpl[T](content: T)
 
-  def apply[T](content: T): IO[T] = IOImpl(content)
-
   def out(message: String): IO[Unit] = IO(println(message))
 
   def in(message: String = ""): IO[String] = IO(readLine(message))
@@ -17,8 +15,10 @@ object IO:
       .foreach((pos, i) => println(s"${i + 1} -> x: ${pos._1} y:${pos._2}"))
     val moveIndex = readLine("Choose your move: ")
     val i = moveIndex.toInt - 1 // TODO: how to handle errors here?
-    IOImpl(availableMoves(i))
+    IO(availableMoves(i))
+
+  def apply[T](content: T): IO[T] = IOImpl(content)
 
   extension [A](gameIO: IO[A])
-    def map[B](f: A => B): IO[B] = IOImpl(f(gameIO.content))
+    def map[B](f: A => B): IO[B] = IO(f(gameIO.content))
     def flatMap[B](f: A => IO[B]): IO[B] = f(gameIO.content)
