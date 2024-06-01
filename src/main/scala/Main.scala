dbcee9436b070f8461a1ca608cd561b2b14643f2
Alessandro Venturini <ales.ventus21@gmail.com>
2024-06-01 17:26:12 +0200

    sorting moves

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,7 +20,7 @@ def play(g: Game): IO[Game] =
         _ <- out(s"Turn: $player\n")
         _ <- out(game.tableAsString)
         _ <- out("\nAvailable moves:")
-        availableMoves = game.availableMoves.toSeq
+        availableMoves = game.availableMoves.toSeq.sorted
         _ <- out(
           availableMoves.zipWithIndex
             .map((pos, i) => s"${i + 1} -> ${pos._1}-${pos._2}")
@@ -31,3 +31,28 @@ def play(g: Game): IO[Game] =
         (newGame, winner) = game.makeMove(availableMoves(moveIndex))
         res <- if (newGame.isFinished) then IO(newGame) else play(newGame)
       yield res
+
+given Ordering[Position] with
+  override def compare(x: Position, y: Position): Int =
+    Ordering[HorizontalPosition].compare(x._1, y._1) match
+      case 0 => Ordering[VerticalPosition].compare(x._2, y._2)
+      case r => r
+
+import HorizontalPosition.{Center as HCenter, *}
+import VerticalPosition.{Center as VCenter, *}
+
+given Ordering[HorizontalPosition] with
+  override def compare(x: HorizontalPosition, y: HorizontalPosition): Int =
+    x match
+      case x if x == y => 0
+      case Left        => -1
+      case HCenter     => if y == Left then 1 else -1
+      case Right       => 1
+
+given Ordering[VerticalPosition] with
+  override def compare(x: VerticalPosition, y: VerticalPosition): Int =
+    x match
+      case x if x == y => 0
+      case Top         => -1
+      case VCenter     => if y == Top then 1 else -1
+      case Bottom      => 1
